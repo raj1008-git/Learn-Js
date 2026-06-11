5a1de1005588b41a31e24e009b70b5f597d86709
raj1008-git <macof.rj@gmail.com>
2026-06-11 21:32:53 +0530

    printing the objects and tables

--- javascript-mastery/phase-1/module1-6.js
+++ javascript-mastery/phase-1/module1-6.js
@@ -15,5 +15,14 @@
 // const allowedRoles=['admin','editor','viewer'];
 // console.log('allowedRoles:', allowedRoles );
 
-console.warn('Warning: Database connection is slow');
-console.error('Error: Could not');
+// console.warn('Warning: Database connection is slow');
+// console.error('Error: Could not');
+
+const users=[
+    {id: 1, name:'Raj',role:'admin'},
+    {id: 2, name:'Priya',role:'editor'},
+    {id: 3, name:'Arjun', role:'Viewer'}
+];
+
+console.log('Plain log', users);
+console.table(users);
