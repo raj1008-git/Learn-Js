fcc337f12f777033ccd852fe4af62818250a3e8c
raj1008-git <macof.rj@gmail.com>
2026-06-11 20:33:03 +0530

    priniting objects

---
+++ javascript-mastery/phase-1/module1-6.js
@@ -0,0 +1,16 @@
+// console.log('Server Started on port', 3000);
+// console.log('Request received:', true, 'Items in queue',5);
+
+const userAge =28;
+console.log('userAge:', userAge);
+
+const userProfile={
+    name: 'Raj',
+    role:'admin',
+    isActive: true
+}
+
+console.log('userProfile:',userProfile);
+
+const allowedRoles=['admin','editor','viewer'];
+console.log('allowedRoles:', allowedRoles );
