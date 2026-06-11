a2293f0b3eb07c9d6b42a21ab45209e4b59b34ee
raj1008-git <macof.rj@gmail.com>
2026-06-11 20:43:33 +0530

    Warnigs and and errors

--- javascript-mastery/phase-1/module1-6.js
+++ javascript-mastery/phase-1/module1-6.js
@@ -1,16 +1,19 @@
 // console.log('Server Started on port', 3000);
 // console.log('Request received:', true, 'Items in queue',5);
 
-const userAge =28;
-console.log('userAge:', userAge);
+// const userAge =28;
+// console.log('userAge:', userAge);
 
-const userProfile={
-    name: 'Raj',
-    role:'admin',
-    isActive: true
-}
+// const userProfile={
+//     name: 'Raj',
+//     role:'admin',
+//     isActive: true
+// }
 
-console.log('userProfile:',userProfile);
+// console.log('userProfile:',userProfile);
 
-const allowedRoles=['admin','editor','viewer'];
-console.log('allowedRoles:', allowedRoles );
+// const allowedRoles=['admin','editor','viewer'];
+// console.log('allowedRoles:', allowedRoles );
+
+console.warn('Warning: Database connection is slow');
+console.error('Error: Could not');
