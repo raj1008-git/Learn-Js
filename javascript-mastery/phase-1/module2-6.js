e450e2ade8eefe2f0bea10c92b80e56775748bed
raj1008-git <macof.rj@gmail.com>
2026-06-12 09:04:01 +0530

    Worked on the const arrays

--- javascript-mastery/phase-1/module2-6.js
+++ javascript-mastery/phase-1/module2-6.js
@@ -1,4 +1,11 @@
-const user = {name: "Raj",age:21};
-user.age=22;
-user.city= "AlipurDuar";
-console.log(user);
+// const user = {name: "Raj",age:21};
+// user.age=22;
+// user.city= "AlipurDuar";
+// console.log(user);
+
+const numbers=[1,2,3];
+
+// numbers=[4,5,6];  //cannot reassign
+numbers.push(4);
+numbers[0]=100;
+console.log(numbers);
