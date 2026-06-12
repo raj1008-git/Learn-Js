e9edcbdd0d1c21c53bececd6fba4b6a11dc23088
raj1008-git <macof.rj@gmail.com>
2026-06-12 10:43:47 +0530

    objects are shared reference, primitives are the independent values

--- javascript-mastery/phase-1/module2-6.js
+++ javascript-mastery/phase-1/module2-6.js
@@ -3,9 +3,20 @@
 // user.city= "AlipurDuar";
 // console.log(user);
 
-const numbers=[1,2,3];
+// const numbers=[1,2,3];
 
-// numbers=[4,5,6];  //cannot reassign
-numbers.push(4);
-numbers[0]=100;
-console.log(numbers);
+// // numbers=[4,5,6];  //cannot reassign
+// numbers.push(4);
+// numbers[0]=100;
+// console.log(numbers);
+
+let x=10;
+let y=x;
+y=20;
+console.log(x); // 10 unchanged.
+
+let obj1={value: 10};
+let obj2= obj1;
+obj2.value=20;
+
+console.log(obj1.value);
